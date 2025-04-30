229708a4715f7e25219c8974e978b19631e29451
MixtliMonroe <nikolai.mixtli@gmail.com>
2025-04-30 18:05:39 +0100

    added a program to save data for a variety of coincidence windows

---
+++ savecounts.py
@@ -0,0 +1,27 @@
+try:
+        import QuTAG_MC
+except:
+        print("Time Tagger wrapper QuTAG.py is not in the search path / same folder.")
+
+import numpy as np
+from main import getData
+
+#Initialize the quTAG device
+qutag = QuTAG_MC.QuTAG()
+
+# Choose diffeent coincidence windows to test (in nanoseconds)
+coincWindow = np.array([1,5,10,15,20,25,30,35,40,45,50])
+# Choose 20 seconds exposure time for all measurements
+Exposure = 20000
+
+# Save the data for different coincidence windows
+for Cwin in coincWindow:
+        print("Cwin: ", Cwin)
+        with open("coincWin_" + str(int(Cwin)) + "ns.txt", "w") as f:
+                counts, timestamps_data, timestamps_channels = getData(qutag, Exposure, int(Cwin*1e3))
+                f.write(str(counts.tolist()) + "\n")
+                f.write(str(timestamps_data.tolist()) + "\n")
+                f.write(str(timestamps_channels.tolist()) + "\n")
+
+# Deinitialize the quTAG device when done
+qutag.deInitialize()
